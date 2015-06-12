2e9fa1416f72d726b829f4d56887ee8e4f0422b2
PCVinnie <vastout@vasnet.nl>
2015-06-12 15:22:34 +0200

    Disabled notes buttontext.

--- HUD.cpp
+++ HUD.cpp
@@ -12,11 +12,12 @@ HUD::HUD() {
 
 void HUD::draw() {
 
+
 	std::string text = GameManager::getInstance()->scene->procedureManager->righternSelectedProcedureObject == nullptr ? "" :
 		GameManager::getInstance()->scene->procedureManager->righternSelectedProcedureObject->name;
 	GameManager::getInstance()->scene->notes->drawNotes(text.data(), text.length(), 200, 300, 0);
 
-	GameManager::getInstance()->scene->notes->drawNotes(buttonText.data(), buttonText.length(), 200, 300, 0);
+	//GameManager::getInstance()->scene->notes->drawNotes(buttonText.data(), buttonText.length(), 200, 300, 0);
 
 }
 
